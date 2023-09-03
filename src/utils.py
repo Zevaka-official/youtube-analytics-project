ee5e395e103f69fc44f5bec795a1cb0b3730def7
Adam <A.ergashev@internet.ru>
2023-09-03 12:35:50 +0300

    Homework_1(yt_api)
    - add utils.py
    - refactor channel.py
    - add pyproject.toml

---
+++ src/utils.py
@@ -0,0 +1,4 @@
+import json
+def printj(dict_to_print: dict) -> None:
+    """Выводит словарь в json-подобном удобном формате с отступами"""
+    print(json.dumps(dict_to_print, indent=2, ensure_ascii=False))
